20576a969ba368c34b3453a2e2e401b553062fb8
Mike Kestemont <mike.kestemont@gmail.com>
2018-01-11 17:23:07 +0100

    base enc-dec running

--- preprocess.py
+++ preprocess.py
@@ -10,11 +10,47 @@ try:
 except:
     print('no NVIDIA driver found')
 
-import seqmod.utils as u
-from seqmod.modules.encoder_decoder import make_rnn_encoder_decoder
+from torch import nn, optim
+
+from skipthoughts import make_rnn_encoder_decoder
+
 from seqmod.misc.dataset import PairedDataset, Dict
+from seqmod.misc import EarlyStopping, Trainer
+from seqmod.misc import StdLogger, VisdomLogger, TensorboardLogger
+from seqmod.misc import PairedDataset, Dict, inflection_sigmoid
+import seqmod.utils as u
 
 from zorro.data import TripleStore
+
+
+def make_encdec_hook(target, gpu, beam=True):
+
+    def hook(trainer, epoch, batch_num, checkpoint):
+        trainer.log("info", "Translating {}".format(target))
+        trg_dict = trainer.model.decoder.embeddings.d
+        scores, hyps = translate(trainer.model, target, gpu, beam=beam)
+        hyps = [u.format_hyp(score, hyp, num + 1, trg_dict)
+                for num, (score, hyp) in enumerate(zip(scores, hyps))]
+        trainer.log("info", '\n***' + ''.join(hyps) + '\n***')
+
+    return hook
+
+
+def make_att_hook(target, gpu, beam=False):
+    assert not beam, "beam doesn't output attention yet"
+
+    def hook(trainer, epoch, batch_num, checkpoint):
+        d = train.decoder.embedding.d
+        scores, hyps, atts = translate(trainer.model, target, gpu, beam=beam)
+        trainer.log("attention",
+                    {"att": atts[0],
+                     "score": sum(scores[0]) / len(hyps[0]),
+                     "target": [d.bos_token] + list(target),
+                     "hyp": ' '.join([d.vocab[i] for i in hyps[0]]).split(),
+                     "epoch": epoch,
+                     "batch_num": batch_num})
+
+    return hook
 
 
 def main():
@@ -25,16 +61,29 @@ def main():
     parser.add_argument('--min_char_freq', type=int, default=10)
     parser.add_argument('--min_len', default=1, type=int)
     parser.add_argument('--max_len', default=15, type=int)
-    parser.add_argument('--dev', default=0.05, type=float)
-    parser.add_argument('--test', default=0.05, type=float)
+    parser.add_argument('--dev', default=0.1, type=float)
     parser.add_argument('--rnd_seed', default=12345, type=int)
     parser.add_argument('--max_triples', default=1000, type=int)
     parser.add_argument('--allow_overlap', action='store_true', default=False)
-    parser.add_argument('--gpu', action='store_true', default=False)
-    parser.add_argument('--reverse', action='store_true', default=False)
 
     # training
-    parser.add_argument('--batch_size', default=15, type=int)
+    parser.add_argument('--epochs', default=5, type=int)
+    parser.add_argument('--gpu', action='store_true')
+    parser.add_argument('--batch_size', default=30, type=int)
+    parser.add_argument('--optim', default='Adam', type=str)
+    parser.add_argument('--lr', default=0.01, type=float)
+    parser.add_argument('--max_norm', default=10., type=float)
+    parser.add_argument('--dropout', default=0.25, type=float)
+    parser.add_argument('--word_dropout', default=0.0, type=float)
+    parser.add_argument('--use_schedule', action='store_true')
+    parser.add_argument('--patience', default=5, type=int)
+    parser.add_argument('--reverse', action='store_true')
+    parser.add_argument('--checkpoint', default=50, type=int)
+    parser.add_argument('--hooks_per_epoch', default=2, type=int)
+    parser.add_argument('--target', default='redrum', type=str)
+    parser.add_argument('--beam', action='store_true')
+    parser.add_argument('--plot', action='store_true')
+
 
     args = parser.parse_args()
 
@@ -43,8 +92,10 @@ def main():
                                allow_overlap=args.allow_overlap,
                                max_triples=args.max_triples)
     triples = list(triple_store)
+    print(f'loaded {len(triple_store)} triples')
 
     # random shuffle:
+    print('shuffling batches...')
     random.seed(args.rnd_seed)
     random.shuffle(triples)
 
@@ -53,21 +104,47 @@ def main():
 
     vocab_dict = Dict(pad_token='<PAD>',
                       min_freq=args.min_char_freq, sequential=True)
-    vocab_dict.fit(left, focus, right) # inefficient?
+    vocab_dict.fit(left, focus, right) # sometimes inefficient? # do a partial fit in the triple store?
     
-    train, dev, test = PairedDataset(
-        src=(left, focus, right, ), trg=None,
-        d={'src': (vocab_dict, vocab_dict, vocab_dict, )},
+    train, valid = PairedDataset(
+        src=(focus,), trg=(left, right),
+        d={'src': (vocab_dict,), 'trg': (vocab_dict, vocab_dict)},
         batch_size=args.batch_size, gpu=args.gpu,
-        align_right=args.reverse, fitted=False).splits(sort_by='src', dev=args.dev, test=args.test, sort=True)
+        align_right=args.reverse, fitted=False).splits(sort_by='src', dev=args.dev, test=None, sort=True)
 
     print(f' * vocabulary size {len(vocab_dict)}')
     print(f' * number of train batches {len(train)}')
+    print(f' * number of dev batches {len(valid)}')
     print(f' * maximum batch size {args.batch_size}')
 
     model = make_rnn_encoder_decoder(2, 64, 150, vocab_dict, cell='GRU', bidi=True, att_type='general')
 
     u.initialize_model(model, rnn={'type': 'orthogonal', 'args': {'gain': 1.0}})
+
+    optimizer = getattr(optim, args.optim)(model.parameters(), lr=args.lr)
+
+    print(model)
+    print('* number of parameters: {}'.format(model.n_params()))
+
+    if args.gpu:
+        model.cuda()
+
+    early_stopping = EarlyStopping(args.patience)
+    trainer = Trainer(
+        model, {'train': train, 'valid': valid}, optimizer,
+        early_stopping=early_stopping, max_norm=args.max_norm)
+    trainer.add_loggers(StdLogger())
+    #trainer.add_loggers(TensorboardLogger(comment='encdec'))
+
+    hook = make_encdec_hook(args.target, args.gpu, beam=args.beam)
+    trainer.add_hook(hook, hooks_per_epoch=args.hooks_per_epoch)
+    hook = u.make_schedule_hook(
+        inflection_sigmoid(len(train) * 2, 1.75, inverse=True))
+    trainer.add_hook(hook, hooks_per_epoch=1000)
+
+    (model, valid_loss), test_loss = trainer.train(
+        args.epochs, args.checkpoint, shuffle=True,
+        use_schedule=args.use_schedule)
 
 if __name__ == '__main__':
     main()
---
+++ skipthoughts.py
@@ -0,0 +1,333 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+from seqmod.misc.beam_search import Beam
+from seqmod.modules.encoder import RNNEncoder, GRLRNNEncoder
+from seqmod.modules.decoder import RNNDecoder
+from seqmod.modules.embedding import Embedding
+from seqmod.modules.torch_utils import flip, shards
+
+from seqmod.modules.exposure import scheduled_sampling
+
+
+class EncoderDecoder(nn.Module):
+    """
+    Configurable encoder-decoder architecture
+
+    Parameters:
+    -----------
+    - encoder: BaseEncoder
+    - decoder: BaseDecoder
+    - exposure_rate: float (0.0, 1.0), initial exposure to model predictions
+        during training.
+    - reverse: bool, whether to run the decoder in reversed order
+    """
+    def __init__(self, encoder, decoder, exposure_rate=1., reverse=False):
+        super(EncoderDecoder, self).__init__()
+
+        self.encoder = encoder
+        self.decoder = decoder
+
+        self.exposure_rate = exposure_rate
+        self.reverse = reverse
+
+        # NLLLoss weight (downweight loss on pad)
+        self.nll_weight = torch.ones(len(self.decoder.embeddings.d))
+        self.nll_weight[self.decoder.embeddings.d.get_pad()] = 0
+
+    def is_cuda(self):
+        "Whether the model is on a gpu. We assume no device sharing."
+        return next(self.parameters()).is_cuda
+
+    def parameters(self, only_trainable=True):
+        """
+        Return trainable parameters
+        """
+        for p in super(EncoderDecoder, self).parameters():
+            if only_trainable and not p.requires_grad:
+                continue
+            yield p
+
+    def n_params(self, only_trainable=True):
+        """
+        Return number of (trainable) parameters
+        """
+        return sum([p.nelement() for p in self.parameters(only_trainable)])
+
+    def freeze_submodule(self, module):
+        """
+        Makes a submodule untrainable
+        """
+        for p in getattr(self, module).parameters():
+            p.requires_grad = False
+
+    def loss(self, batch_data, test=False, split=25, use_schedule=False):
+        """
+        Return batch-averaged loss and examples processed for speed monitoring
+
+        Parameters:
+        -----------
+
+        - split: int, max targets per binned softmax loss computation
+        - use_schedule: bool, whether to use scheduled sampling when computing
+            the decoder loss. The rate of sampling is defined by the
+            instance variable `exposure_rate`.
+        """
+        # unpack batch data
+        (src, trgs), (src_conds, trg_conds) = batch_data, (None, None)
+
+        src = src[0]
+
+        # limit to only right target
+        trg = trgs[1]
+
+        if self.encoder.conditional:
+            (src, *src_conds) = src
+        (src, src_lengths) = src
+        if self.decoder.conditional:
+            (trg, *trg_conds) = trg
+        (trg, trg_lengths) = trg
+
+        # word-level loss weight (weight down padding)
+        weight = self.nll_weight
+        if self.is_cuda():
+            weight = weight.cuda()
+
+        # remove <eos> from decoder targets, remove <bos> from loss targets
+        if hasattr(self, 'reverse') and self.reverse:
+            # assume right aligned data: [pad pad <bos> ... <eos>]
+            trg = flip(trg, 0)
+
+        # [<bos> ... <eos> pad pad]
+        dec_trg, loss_trg = trg[:-1], trg[1:]
+
+        # compute model output
+        enc_outs, enc_hidden = self.encoder(src, lengths=src_lengths)
+        dec_state = self.decoder.init_state(
+            enc_outs, enc_hidden, src_lengths, conds=trg_conds)
+
+        dec_outs = []
+        for step, t in enumerate(dec_trg):
+            if use_schedule and step > 0 and self.exposure_rate < 1.0:
+                t = scheduled_sampling(
+                    t, dec_outs[-1],
+                    self.decoder.project,
+                    self.exposure_rate)
+                t = Variable(t, volatile=not self.training)
+            out, _ = self.decoder(t, dec_state)
+            dec_outs.append(out)
+        dec_outs = torch.stack(dec_outs)
+
+        # compute loss and backprop
+        enc_losses, _ = self.encoder.loss(enc_outs, src_conds, test=test)
+
+        # compute memory efficient word loss
+        shard_data = {'out': dec_outs, 'trg': loss_trg}
+        num_examples, loss = trg_lengths.data.sum(), 0
+
+        for shard in shards(shard_data, size=split, test=test):
+            out, true = shard['out'], shard['trg'].view(-1)
+            pred = self.decoder.project(out)
+            shard_loss = F.nll_loss(pred, true, weight, size_average=False)
+            shard_loss /= num_examples
+            loss += shard_loss
+
+            if not test:
+                shard_loss.backward(retain_graph=True)
+
+        return (loss.data[0], *enc_losses), num_examples
+
+    def translate(self, src, lengths, conds=None, max_decode_len=2):
+        """
+        Translate a single input sequence using greedy decoding.
+
+        Parameters:
+        -----------
+
+        src: torch.LongTensor (seq_len x batch_size)
+
+        Returns (scores, hyps, atts):
+        --------
+        scores: (batch_size)
+        hyps: (batch_size x trg_seq_len)
+        atts: (batch_size x trg_seq_len x source_seq_len)
+        """
+        eos = self.decoder.embeddings.d.get_eos()
+        bos = self.decoder.embeddings.d.get_bos()
+        if hasattr(self, 'reverse') and self.reverse:
+            bos, eos = eos, bos
+        seq_len, batch_size = src.size()
+
+        enc_outs, enc_hidden = self.encoder(src)
+        dec_state = self.decoder.init_state(
+            enc_outs, enc_hidden, lengths, conds=conds)
+
+        hyps, weights, scores = [], [], 0
+        mask = src.data.new(batch_size).zero_().float() + 1
+        prev = Variable(src.data.new([bos]).expand(batch_size), volatile=True)
+
+        for _ in range(len(src) * max_decode_len):
+            out, weight = self.decoder(prev, dec_state)
+            # decode
+            logprobs = self.decoder.project(out)
+            logprobs, prev = logprobs.max(1)
+            # accumulate
+            hyps.append(prev.data)
+            if self.decoder.has_attention:
+                weights.append(weight.data)
+            scores += logprobs.data
+            # update mask
+            mask = mask * (prev.data != eos).float()
+
+            if mask.sum() == 0:
+                break
+
+        hyps = torch.stack(hyps).transpose(0, 1).tolist()  # batch first
+        scores = scores.tolist()
+        if self.decoder.has_attention:
+            weights = torch.stack(weights).tolist()
+
+        if hasattr(self, 'reverse') and self.reverse:
+            hyps = [hyp[::-1] for hyp in hyps]
+
+        return scores, hyps, weights
+
+    def translate_beam(self, src, lengths, mask=None, conds=None,
+                       beam_width=5, max_decode_len=2):
+        """
+        Translate a single input sequence using beam search.
+
+        Parameters:
+        -----------
+
+        src: torch.LongTensor (seq_len x 1)
+        lengths: torch.LongTensor (1)
+        mask: (optional) see Decoder.init_state
+        conds: (optional) conditions for the decoder
+        """
+        eos = self.decoder.embeddings.d.get_eos()
+        bos = self.decoder.embeddings.d.get_bos()
+        if hasattr(self, 'reverse') and self.reverse:
+            bos, eos = eos, bos
+        gpu = src.is_cuda
+
+        weights = []
+
+        enc_outs, enc_hidden = self.encoder(src)
+        dec_state = self.decoder.init_state(
+            enc_outs, enc_hidden, lengths, conds=conds)
+
+        dec_state.expand_along_beam(beam_width)
+
+        beam = Beam(beam_width, bos, eos=eos, gpu=gpu)
+
+        while beam.active and len(beam) < len(src) * max_decode_len:
+            # (width) -> (1 x width)
+            prev = Variable(beam.get_current_state(), volatile=True)
+            dec_out, weight = self.decoder(prev, dec_state)
+            logprobs = self.decoder.project(dec_out)  # (width x vocab_size)
+            beam.advance(logprobs.data)
+            dec_state.reorder_beam(beam.get_source_beam())
+            # TODO: add attention weight for decoded steps
+
+        scores, hyps = beam.decode(n=beam_width)
+        if hasattr(self, 'reverse') and self.reverse:
+            hyps = [hyp[::-1] for hyp in hyps]
+
+        return scores, hyps, weights
+
+
+def make_embeddings(src_dict, trg_dict, emb_dim, word_dropout):
+    src_embeddings = Embedding.from_dict(src_dict, emb_dim)
+    if trg_dict is not None:
+        trg_embeddings = Embedding.from_dict(trg_dict, emb_dim, p=word_dropout)
+    else:
+        trg_embeddings = Embedding.from_dict(src_dict, emb_dim, p=word_dropout)
+        trg_embeddings.weight = src_embeddings.weight
+
+    return src_embeddings, trg_embeddings
+
+
+def make_rnn_encoder_decoder(
+        num_layers,
+        emb_dim,
+        hid_dim,
+        src_dict,
+        trg_dict=None,
+        cell='LSTM',
+        bidi=True,
+        encoder_summary='full',
+        att_type=None,
+        dropout=0.0,
+        input_feed=True,
+        word_dropout=0.0,
+        deepout_layers=0,
+        deepout_act='ReLU',
+        tie_weights=False,
+        reuse_hidden=True,
+        train_init=False,
+        add_init_jitter=False,
+        cond_dims=None,
+        cond_vocabs=None,
+        reverse=False
+):
+    """
+    - num_layers: int, Number of layers for both the encoder and the decoder.
+    - emb_dim: int, Embedding dimension.
+    - hid_dim: int, Hidden state size for the encoder and the decoder.
+    - src_dict: Dict, A fitted Dict used to encode the data into integers.
+    - trg_dict: Dict, Same as src_dict in case of bilingual training.
+    - cell: string, Cell type to use. One of (LSTM, GRU).
+    - bidi: bool, Whether to use bidirectional encoder.
+    - encoder_summary: How to compute summary vector for the decoder.
+    - att_type: string, Attention mechanism to use.
+    - dropout: float
+    - input_feed: bool,
+        Whether to feed back the previous context as input to the decoder
+        for the next step together with the last predicted word embedding.
+    - word_dropout: float
+    - deepout_layers: int, Whether to use a highway layer before the output
+        projection in the decoder.
+    - deepout_act: str, Non-linear activation in the deepout layer if given.
+    - tie_weights: bool, Whether to tie embedding input and output weights.
+        It wouldn't make much sense in bilingual settings.
+    - reuse_hidden: bool, whether to reuse encoder hidden for initializing the
+        decoder.
+    - train_init: bool, whether to train the initial hidden state of both
+        encoder and decoder.
+    - add_init_jitter: bool, whether to add gaussian noise to the initial
+        hidden state.
+    - cond_dims: tuple of integers with the embedding dimension corresponding
+        to each condition.
+    - cond_vocabs: tuple of integers with the number of classes for each
+        condition in same order as `cond_dims`.
+    """
+    src_embeddings, trg_embeddings = make_embeddings(
+        src_dict, trg_dict, emb_dim, word_dropout)
+
+    encoder = RNNEncoder(src_embeddings, hid_dim, num_layers, cell=cell,
+                         bidi=bidi, dropout=dropout, summary=encoder_summary,
+                         train_init=False, add_init_jitter=False)
+
+    encoder_dims, encoder_size = encoder.encoding_size
+
+    decoder = RNNDecoder(trg_embeddings, hid_dim, num_layers, cell, encoder_size,
+                         dropout=dropout, input_feed=input_feed,
+                         att_type=att_type, deepout_layers=deepout_layers,
+                         deepout_act=deepout_act,
+                         tie_weights=tie_weights, reuse_hidden=reuse_hidden,
+                         train_init=train_init,
+                         add_init_jitter=add_init_jitter,
+                         cond_dims=cond_dims, cond_vocabs=cond_vocabs)
+
+    if decoder.has_attention:
+        if encoder_summary != 'full':
+            raise ValueError("Attentional decoder needs full encoder summary")
+    else:
+        if encoder_dims != 2:
+            raise ValueError("Non attentional decoder needs 2D encoding")
+
+    return EncoderDecoder(encoder, decoder, reverse=reverse)
