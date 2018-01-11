3bd8eef4bc8449597376bc5fa99efda090349fa8
Mike Kestemont <mike.kestemont@gmail.com>
2018-01-11 09:41:48 +0100

    added char-level shingling

--- zorro/data.py
+++ zorro/data.py
@@ -6,7 +6,8 @@ from collections import deque
 class TripleStore(object):
 
     def __init__(self, input_, shingling='sentences',
-                 size=100, allow_overlap=False, max_triples=None):
+                 size=100, allow_overlap=False, max_triples=None,
+                 left_size=0, focus_size=0, right_size=0, shingle_stride=0):
         """
         size is the number of characters in a single when `shingling=characters.
         """
@@ -19,8 +20,21 @@ class TripleStore(object):
 
         if shingling not in ('sentences', 'characters'):
             raise ValueError(f'Unsupported shingling option {shingling}')
+        if shingling == 'characters':
+            if not focus_size:
+                raise ValueError(f'With character shingling option, focus_size should be >=1')
+            if not left_size and not right_size:
+                raise ValueError(f'At least, left_size OR right_size should be specified.')
+        if not shingle_stride:
+            shingle_stride = focus_size
+        if shingle_stride > left_size + focus_size + right_size:
+            raise ValueError(f'shingle_stride too large: you are missing out on data.')
 
         self.shingling = shingling
+        self.focus_size = focus_size
+        self.left_size = left_size
+        self.right_size = right_size
+        self.shingle_stride = shingle_stride
         self.allow_overlap = allow_overlap
         self.max_triples = max_triples
         self.processed = 0
@@ -54,8 +68,34 @@ class TripleStore(object):
                             return
 
         elif self.shingling == 'characters':
-            raise NotImplementedError
+            chunk_size = self.left_size + self.focus_size + self.right_size
+            for filename in self.filenames:
+                chunk = ''
+                for line in open(filename, 'r'):
+                    line = line.strip()
+                    if line:
+                        chunk += ' ' + line
+                    while len(chunk) >= chunk_size:
+                        yield tuple([chunk[:self.left_size],
+                                     chunk[self.left_size : self.left_size + self.focus_size],
+                                     chunk[self.left_size + self.focus_size : -(len(chunk) - chunk_size)]])
+                        if self.allow_overlap:
+                            chunk = chunk[self.shingle_stride:]
+                        else:
+                            chunk = chunk[-(len(chunk) - chunk_size):]
 
     def __len__(self):
         # number of triples yielded so far
         return self.processed
+
+"""
+def main():
+    triple_store = TripleStore(input_='../data', shingling='characters',
+                 size=100, allow_overlap=True, max_triples=10,
+                 focus_size=21, left_size=21, right_size=21)
+    for triple in triple_store:
+        print('\t'.join([t for t in triple]))
+
+if __name__ == '__main__':
+    main()
+"""
